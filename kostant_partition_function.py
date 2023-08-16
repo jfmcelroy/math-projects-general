3f35b3d4b0e5801f2d2bbf6eddcb5a921aaa1cd5
James McElroy <105364772+jfmcelroy@users.noreply.github.com>
2023-08-16 15:43:02 -0400

    Update kostant_partition_function.py

--- kostant_partition_function.py
+++ kostant_partition_function.py
@@ -2,5 +2,7 @@
 #
 # goal: compute values for kostant partition function
 #
-# input: n: length, S_G: multiset of type A_n roots, a: length n integer vector
+# input: n: length, S: multiset of type A_(n-1) roots, a: length n integer vector
 # output: nonnegative integer (representing the number of nonnegative integer linear combinations of S_G that equate to a)
+
+'''Compute the Kostant partition function K_G(a) for a multiset S of type A_{n-1} roots'''
