30d4eb95ccb2ed7f074cfa274d5fc4268b5aca4b
James McElroy <105364772+jfmcelroy@users.noreply.github.com>
2023-08-16 15:54:00 -0400

    Update multiset_practice.py

--- multiset_practice.py
+++ multiset_practice.py
@@ -4,10 +4,10 @@
 from multiset import *
  
 # print empty multiset
-print(Multiset())
+# print(Multiset())
  
 # print multiset from iterable
-print(Multiset('abcde'))
+# print(Multiset('abcde'))
  
 # print multiset from mapping
-print(Multiset({(1,-1,0,0): 2, {(1,0,0,-1): 1, (0,1,-1,0): 2, (0,0,1,-1): 2}))
+print(Multiset({(1,-1,0,0): 2, (1,0,0,-1): 1, (0,1,-1,0): 2, (0,0,1,-1): 2}))
