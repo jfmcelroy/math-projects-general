9de17719696340713e359a05431c9a85b2e9bd75
James McElroy <105364772+jfmcelroy@users.noreply.github.com>
2023-08-16 15:44:40 -0400

    Create multiset_practice.py

---
+++ multiset_practice.py
@@ -0,0 +1,13 @@
+# #multiset_practice.py
+#
+# practicing the multiset package
+from multiset import *
+ 
+# print empty multiset
+print(Multiset())
+ 
+# print multiset from iterable
+print(Multiset('abcde'))
+ 
+# print multiset from mapping
+print(Multiset({'a': 4, 'b': 2, 'c': 3, 'd':1}))
