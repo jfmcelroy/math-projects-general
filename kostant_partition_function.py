28c16377cc0c7cee128845e5571d67da8f1f776a
James McElroy <105364772+jfmcelroy@users.noreply.github.com>
2023-08-16 18:12:37 -0400

    Update kostant_partition_function.py

--- kostant_partition_function.py
+++ kostant_partition_function.py
@@ -8,11 +8,6 @@
 # from multiset import * # this package may not be necessary
 import numpy as np
 
-# target vector(s)
-netflows = [np.array([2,-1,-1,0]), np.array([1,0,-1,0]), np.array([0,1,-1,0]), np.array([1,-1,0,0]), np.array([0,0,0,0])]
-for a in netflows:
-  print('target vector:', a)
-
 # multiset of type A_n roots based on the edge set of G
 # for now, enter this manually
 e1 = np.array([1,-1,0,0])
@@ -35,23 +30,29 @@ for i in range(7):
 u = 2
 print('largest allowed coefficient:', u)
 
-# computing nonnegative integer linear combinations 
-# for now, run some for loops and then count matches afterwords
-combo = np.array([0,0,0,0]) # stores the result of the nonnegative integer linear combination 
-match = 0 # counter for matches
-for i in range(u+1):
-  for j in range(u+1):
-    for k in range(u+1):
-      for m in range(u+1):
-        for n in range(u+1):
-          for o in range(u+1):
-            for p in range(u+1):
-              combo = i*l[0]+j*l[1]+k*l[2]+m*l[3]+n*l[4]+o*l[5]+p*l[6]
-              if np.array_equal(combo,a) == True:
-                print(i,j,k,m,n,o,p)
-                match += 1
+# target vector(s)
+netflows = [np.array([2,-1,-1,0]), np.array([1,0,-1,0]), np.array([0,1,-1,0]), np.array([1,-1,0,0]), np.array([0,0,0,0])]
+for a in netflows:
+  print('target vector:', a)
+  combo = np.array([0,0,0,0]) # stores the result of the nonnegative integer linear combination 
+  match = 0 # counter for matches
+  for i in range(u+1):
+    for j in range(u+1):
+      for k in range(u+1):
+        for m in range(u+1):
+          for n in range(u+1):
+            for o in range(u+1):
+              for p in range(u+1):
+                combo = i*l[0]+j*l[1]+k*l[2]+m*l[3]+n*l[4]+o*l[5]+p*l[6]
+                if np.array_equal(combo,a) == True:
+                  print(i,j,k,m,n,o,p)
+                  match += 1
 
 print('K_G(', a, ')=', match)
+
+
+# computing nonnegative integer linear combinations 
+# for now, run some for loops and then count matches afterwords
 
 
 ###### HEY DON'T FORGET LAST ENTRY OF a SHOULD ALWAYS BE 0 #######
