7e38ce1ec4c600d693bd463efc4cc794e5af7b1e
James McElroy <105364772+jfmcelroy@users.noreply.github.com>
2023-08-16 16:06:44 -0400

    Update multiset_practice.py

--- multiset_practice.py
+++ multiset_practice.py
@@ -2,6 +2,7 @@
 #
 # practicing the multiset package
 from multiset import *
+import numpy as np
  
 # print empty multiset
 # print(Multiset())
@@ -10,4 +11,11 @@ from multiset import *
 # print(Multiset('abcde'))
  
 # print multiset from mapping
-print(Multiset({(1,-1,0,0): 2, (1,0,0,-1): 1, (0,1,-1,0): 2, (0,0,1,-1): 2}))
+# print(Multiset({(1,-1,0,0): 2, (1,0,0,-1): 1, (0,1,-1,0): 2, (0,0,1,-1): 2}))
+
+# vector algebra
+e1 = np.array([1,0,0,0])
+e2 = np.array([0,1,0,0])
+e3 = np.array([0,0,1,0])
+e4 = np.array([0,0,0,1])
+print(e1,e2,e3,e4)
